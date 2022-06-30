f35ee5a749b72d751a7b33907093b974cf9fdd1a
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 17:11:49 -1000

    Create react app for color box maker

---
+++ color-box-maker/src/BoxList.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const BoxList = () => {
+	const [boxes, setBoxes] = [
+		{ width: "100px", height: "100px", color: "green" },
+	];
+	return <div>BoxList</div>;
+};
+
+export default BoxList;
