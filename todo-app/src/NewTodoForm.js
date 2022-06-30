70582c1fb8378ed5350c96d8513681c5f0fb8585
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 21:19:14 -1000

    Create NewTodoForm

---
+++ todo-app/src/NewTodoForm.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+const NewTodoForm = ({ addTodo }) => {
+	const [taskInput, setTaskInput] = useState("");
+	const handleChange = (event) => {
+		event.preventDefault();
+		setTaskInput(event.target.value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		addTodo({ id: uuid, task: taskInput });
+		setTaskInput("");
+	};
+	return (
+		<form className="NewTodoForm" onSubmit={handleSubmit}>
+			<label htmlFor="task">Task</label>
+			<input
+				type="text"
+				id="task"
+				name="task"
+				placeholder="Todo"
+				value={taskInput}
+				onChange={handleChange}
+			/>
+			<button>Add New Todo!</button>
+		</form>
+	);
+};
+
+export default NewTodoForm;
