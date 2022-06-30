b2a27163c8703a0d3b41513854e5df5fce181561
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 17:14:24 -1000

    Map over boxes in state and render Box components

--- color-box-maker/src/BoxList.js
+++ color-box-maker/src/BoxList.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
 
 const BoxList = () => {
 	const [boxes, setBoxes] = [
 		{ width: "100px", height: "100px", color: "green" },
 	];
-	return <div>BoxList</div>;
+	return (
+		<div>
+			<h1>Color Box Maker</h1>
+			{boxes.map((box) => (
+				<Box width={box.width} height={box.height} color={box.color} />
+			))}
+		</div>
+	);
 };
 
 export default BoxList;
