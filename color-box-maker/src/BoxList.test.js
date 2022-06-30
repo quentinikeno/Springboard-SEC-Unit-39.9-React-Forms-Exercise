32064c056acfc690be12437bf81ad0c56211d066
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 18:39:46 -1000

    Add a function to create a new Box

--- color-box-maker/src/BoxList.test.js
+++ color-box-maker/src/BoxList.test.js
@@ -1,5 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import BoxList from "./BoxList";
+
+const addNewBox = (
+	boxList,
+	width = "100",
+	height = "100",
+	backgroundColor = "green"
+) => {
+	const widthInput = boxList.getByLabelText("Width");
+	const heightInput = boxList.getByLabelText("Height");
+	const backgroundColorInput = boxList.getByLabelText("Background Color");
+	fireEvent.change(widthInput, { target: width });
+	fireEvent.change(heightInput, { target: height });
+	fireEvent.change(backgroundColorInput, { target: backgroundColor });
+	const submitButton = boxList.getByText("Create a new box!");
+	fireEvent.click(submitButton);
+};
 
 test("rendering without crashing", () => {
 	render(<BoxList />);
