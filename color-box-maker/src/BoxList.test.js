beb6ab4dc16754fb154ce899aec4fbbbc64a3e19
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 19:01:55 -1000

    Add test for adding new box

--- color-box-maker/src/BoxList.test.js
+++ color-box-maker/src/BoxList.test.js
@@ -10,9 +10,11 @@ const addNewBox = (
 	const widthInput = boxList.getByLabelText("Width");
 	const heightInput = boxList.getByLabelText("Height");
 	const backgroundColorInput = boxList.getByLabelText("Background Color");
-	fireEvent.change(widthInput, { target: width });
-	fireEvent.change(heightInput, { target: height });
-	fireEvent.change(backgroundColorInput, { target: backgroundColor });
+	fireEvent.change(widthInput, { target: { value: width } });
+	fireEvent.change(heightInput, { target: { value: height } });
+	fireEvent.change(backgroundColorInput, {
+		target: { value: backgroundColor },
+	});
 	const submitButton = boxList.getByText("Create a new box!");
 	fireEvent.click(submitButton);
 };
@@ -24,4 +26,30 @@ test("rendering without crashing", () => {
 test("matching the snapshot", () => {
 	const { asFragment } = render(<BoxList />);
 	expect(asFragment()).toMatchSnapshot();
+});
+
+test("Adding a new Box", () => {
+	const boxList = render(<BoxList />);
+
+	// No delete buttons for Boxes in the DOM
+	expect(boxList.queryByText("X")).not.toBeInTheDocument();
+
+	// Add a new Box
+	addNewBox(boxList);
+
+	// A delete button for the new box should be in the DOM
+	const removeButton = boxList.getByText("X");
+	expect(removeButton).toBeInTheDocument();
+
+	// The div should have styles
+	expect(removeButton.previousSibling).toHaveStyle(`
+    width: 100px;
+    height: 100px;
+    background-color: green;
+  `);
+
+	// expect form inputs to reset
+	expect(boxList.getByLabelText("Width")).toHaveValue("250");
+	expect(boxList.getByLabelText("Height")).toHaveValue("250");
+	expect(boxList.getByLabelText("Background Color")).toHaveValue("");
 });
