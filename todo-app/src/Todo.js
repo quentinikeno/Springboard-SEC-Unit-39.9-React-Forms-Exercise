6a108a16551c08c97bde6e696fc49fec845f6eec
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 21:27:44 -1000

    Add handle click to remove Todo when button is clicked

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -1,8 +1,13 @@
-const Todo = ({ task }) => {
+const Todo = ({ id, task, removeTodo }) => {
+	const handleClick = () => {
+		removeTodo(id);
+	};
 	return (
 		<div>
 			{task}
-			<button className="Todo-delete-btn">X</button>
+			<button className="Todo-delete-btn" onClick={handleClick}>
+				X
+			</button>
 		</div>
 	);
 };
