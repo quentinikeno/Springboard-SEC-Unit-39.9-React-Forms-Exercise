0ea2405606e8770b13a1fca4fe79330ef3db6269
Quentin Ikeno <ikenoq@gmail.com>
2022-06-29 18:06:22 -1000

    Add button to remove Box from DOM

--- color-box-maker/src/Box.js
+++ color-box-maker/src/Box.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 const Box = ({ id, height, width, backgroundColor, removeBox }) => {
+	const handleRemove = () => {
+		removeBox(id);
+	};
 	return (
-		<div
-			style={{
-				height: `${height}px`,
-				width: `${width}px`,
-				backgroundColor,
-			}}
-		></div>
+		<div>
+			<div
+				style={{
+					height: `${height}px`,
+					width: `${width}px`,
+					backgroundColor,
+				}}
+			></div>
+			<button onClick={handleRemove}>X</button>
+		</div>
 	);
 };
 
